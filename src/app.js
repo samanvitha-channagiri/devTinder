109143c9dc1fd5d590f9cef5022a4e5997a1fd14
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-03-31 12:29:11 +0530

    understanding the params and queries

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express()
 
-app.get('/user/:userId',(req,res)=>{
+app.get('/user/:userid/:username/:userpass',(req,res)=>{
         res.send({firstName:"sam",lastName:"Sam"})
         console.log(req.params);
         console.log(req.query);
